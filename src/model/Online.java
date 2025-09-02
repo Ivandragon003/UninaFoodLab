35eb2c861ed813e12dff5cceacf7ed3aa237fc5b
Matrbo7 <mattiacerbone7@gmail.com>
2025-09-02 18:10:35 +0200

    classi

--- src/model/Online.java
+++ src/model/Online.java
@@ -1,5 +1,22 @@
 package model;
 
-public class Online {
+import java.time.LocalDateTime;
 
+public class Online extends Sessione {
+
+	public Online(LocalDateTime dataInizioSessione, LocalDateTime dataFineSessione) {
+		super(dataInizioSessione, dataFineSessione);
+	}
+
+	private String piattaformaStreaming;
+
+	public String getPiattaformaStreaming() {
+		return piattaformaStreaming;
+	}
+
+	public void setPiattaformaStreaming(String piattaformaStreaming) {
+		this.piattaformaStreaming = piattaformaStreaming;
+	}
+	
+	
 }
--- src/model/Sessione.java
+++ src/model/Sessione.java
@@ -1,5 +1,37 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Sessione {
+
+private int idSessione;
+private LocalDateTime dataInizioSessione;
+private LocalDateTime dataFineSessione;
+
+//VA CREATA LA ENUM TIPO
+
+
+
+public Sessione(LocalDateTime dataInizioSessione, LocalDateTime dataFineSessione) {
+	super();
+	this.dataInizioSessione = dataInizioSessione;
+	this.dataFineSessione = dataFineSessione;
+}
+
+public LocalDateTime getDataInizioSessione() {
+	return dataInizioSessione;
+}
+
+public void setDataInizioSessione(LocalDateTime dataInizioSessione) {
+	this.dataInizioSessione = dataInizioSessione;
+}
+
+public LocalDateTime getDataFineSessione() {
+	return dataFineSessione;
+}
+
+public void setDataFineSessione(LocalDateTime dataFineSessione) {
+	this.dataFineSessione = dataFineSessione;
+}
 
 }
