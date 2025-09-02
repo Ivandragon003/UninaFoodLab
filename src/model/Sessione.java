f536afb2687e3217dab8f2bbb97094a4f21167af
Matrbo7 <mattiacerbone7@gmail.com>
2025-09-02 18:48:19 +0200

    Update Sessione.java

--- src/model/Sessione.java
+++ src/model/Sessione.java
@@ -1,4 +1,8 @@
+package model;
+import java.time.LocalDateTime;
+
 public class Sessione {
 	private int idSessione;
-	private 
+	private LocalDateTime dataInizioSessione;
+	private LocalDateTime dataFineSessione;
 }
