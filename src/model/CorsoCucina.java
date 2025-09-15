1f9f85310cee93f51315fbf4150ac63bd7b1ebd0
Ivandragon003 <ivandefelice05@gmail.com>
2025-09-15 14:55:24 +0200

    ripristino

--- src/model/CorsoCucina.java
+++ src/model/CorsoCucina.java
@@ -1,6 +1,5 @@
 package model;
 
-<<<<<<< Updated upstream
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,18 +18,16 @@ public class CorsoCucina {
 
 	
 	private List<Iscrizione> iscrizioni = new ArrayList<>();
-
+	private List<Sessione> sessioni = new ArrayList<>();
 	
 	public CorsoCucina(int idCorso, String nomeCorso, double prezzo, String categoria, Frequenza frequenzaCorso,
-			int numeroPosti, int numeroSessioni, LocalDateTime dataInizioCorso, LocalDateTime dataFineCorso) {
+			int numeroPosti, int numeroSessioni) {
 		setNomeCorso(nomeCorso);
 		setPrezzo(prezzo);
 		setCategoria(categoria);
 		setFrequenzaCorso(frequenzaCorso);
 		setNumeroPosti(numeroPosti);
 		setNumeroSessioni(numeroSessioni);
-		setDataInizioCorso(dataInizioCorso);
-		setDataFineCorso(dataFineCorso);
 	}
 
 	// Getter e Setter
@@ -79,6 +76,14 @@ public class CorsoCucina {
 		this.frequenzaCorso = frequenzaCorso;
 	}
 
+	public List<Sessione> getSessioni() {
+		return sessioni;
+	}
+
+	public void setSessioni(List<Sessione> sessioni) {
+		this.sessioni = sessioni;
+	}
+
 	public int getNumeroPosti() {
 		return numeroPosti;
 	}
@@ -110,7 +115,7 @@ public class CorsoCucina {
 			throw new IllegalArgumentException("Data inizio corso non può essere nel passato");
 		this.dataInizioCorso = dataInizioCorso;
 	}
-
+	
 	public LocalDateTime getDataFineCorso() {
 		return dataFineCorso;
 	}
@@ -123,6 +128,17 @@ public class CorsoCucina {
 		this.dataFineCorso = dataFineCorso;
 	}
 
+	// Imposta la data inizio senza controlli (usato solo dal DAO)
+	public void setDataInizioCorsoFromDB(LocalDateTime dataInizioCorso) {
+	    this.dataInizioCorso = dataInizioCorso;
+	}
+
+	// Imposta la data fine senza controlli (usato solo dal DAO)
+	public void setDataFineCorsoFromDB(LocalDateTime dataFineCorso) {
+	    this.dataFineCorso = dataFineCorso;
+	}
+
+	
 	
 	public void aggiungiIscrizione(Iscrizione i) {
 		if (!iscrizioni.contains(i))
@@ -175,106 +191,3 @@ public class CorsoCucina {
 	}
 
 }
-=======
-	import java.time.LocalDateTime;
-
-	public class CorsoCucina {
-
-	private String idCorso;
-	private String nomeCorso;
-	private double prezzo;
-	private String categoria;
-	
-	private enum Frequenza {
-		GIORNALIERO("Una lezione ogni giorno"),
-		SETTIMANALE("Una lezione a settimana"),
-		MENSILE("Una lezione al mese");
-		
-		private String descrizione;
-		
-		//COSTRUTTORE
-		private Frequenza(String descrizione) {
-			this.descrizione = descrizione;		
-		}
-		
-		private String getDescrizione() {
-			return descrizione;
-		}
-	}
-
-	private Frequenza frequenzaCorso;
-
-	private int numeroPosti;
-	private int numeroSessioni;
-	private LocalDateTime dataInizioCorso;
-	private LocalDateTime dataFineCorso;
-
-
-
-	public CorsoCucina(String idCorso, String nomeCorso, double prezzo, String categoria, Frequenza frequenzaCorso,
-			int numeroPosti, int numeroSessioni, LocalDateTime dataInizioCorso, LocalDateTime dataFineCorso) {
-		super();
-		this.idCorso = idCorso;
-		this.nomeCorso = nomeCorso;
-		this.prezzo = prezzo;
-		this.categoria = categoria;
-		this.frequenzaCorso = frequenzaCorso;
-		this.numeroPosti = numeroPosti;
-		this.numeroSessioni = numeroSessioni;
-		this.dataInizioCorso = dataInizioCorso;
-		this.dataFineCorso = dataFineCorso;
-	}
-
-
-	public String getIdCorso() {
-		return idCorso;
-	}
-	public void setIdCorso(String idCorso) {
-		this.idCorso = idCorso;
-	}
-	public String getNomeCorso() {
-		return nomeCorso;
-	}
-	public void setNomeCorso(String nomeCorso) {
-		this.nomeCorso = nomeCorso;
-	}
-	public double getPrezzo() {
-		return prezzo;
-	}
-	public void setPrezzo(double prezzo) {
-		this.prezzo = prezzo;
-	}
-	public String getCategoria() {
-		return categoria;
-	}
-	public void setCategoria(String categoria) {
-		this.categoria = categoria;
-	}
-	public int getNumeroPosti() {
-		return numeroPosti;
-	}
-	public void setNumeroPosti(int numeroPosti) {
-		this.numeroPosti = numeroPosti;
-	}
-	public int getNumeroSessioni() {
-		return numeroSessioni;
-	}
-	public void setNumeroSessioni(int numeroSessioni) {
-		this.numeroSessioni = numeroSessioni;
-	}
-	public LocalDateTime getDataInizioCorso() {
-		return dataInizioCorso;
-	}
-	public void setDataInizioCorso(LocalDateTime dataInizioCorso) {
-		this.dataInizioCorso = dataInizioCorso;
-	}
-	public LocalDateTime getDataFineCorso() {
-		return dataFineCorso;
-	}
-	public void setDataFineCorso(LocalDateTime dataFineCorso) {
-		this.dataFineCorso = dataFineCorso;
-	}
-
-	}
-
->>>>>>> Stashed changes
--- src/model/Sessione.java
+++ src/model/Sessione.java
@@ -2,7 +2,7 @@ package model;
 
 import java.time.LocalDateTime;
 
-public class Sessione {
+abstract public class Sessione {
 	private int idSessione;
 	private LocalDateTime dataInizioSessione;
 	private LocalDateTime dataFineSessione;
