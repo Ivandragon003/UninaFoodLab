8b7e1f59004ae3a9ce82f8f5f942149481624a47
Ivandragon003 <ivandefelice05@gmail.com>
2025-09-18 19:07:55 +0200

    risolto problema libreria

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,30 @@
+// NESSUN package - metti Main.java direttamente in src/
+
 import controller.ChefController;
 import service.GestioneChef;
 import dao.ChefDAO;
 import dao.LavoraDAO;
 import dao.TieneDAO;
-
 import javafx.application.Application;
 import Gui.LoginChefGUI;
 
 public class Main {
-	public static void main(String[] args) {
-		// Inizializza DAO e Service
-		ChefDAO chefDAO = new ChefDAO();
-		LavoraDAO lavoraDAO = new LavoraDAO();
-		TieneDAO tieneDAO = new TieneDAO();
-		GestioneChef gestioneChef = new GestioneChef(chefDAO, lavoraDAO, tieneDAO);
-		// Inizializza Controller
-		ChefController chefController = new ChefController(gestioneChef);
-
-		// Passa il controller alla GUI tramite setter statico
-		LoginChefGUI.setController(chefController);
-
-		// Avvia JavaFX
-		Application.launch(LoginChefGUI.class, args);
-	}
+    public static void main(String[] args) {
+      
+        
+        // Inizializza DAO e Service
+        ChefDAO chefDAO = new ChefDAO();
+        LavoraDAO lavoraDAO = new LavoraDAO();
+        TieneDAO tieneDAO = new TieneDAO();
+        GestioneChef gestioneChef = new GestioneChef(chefDAO, lavoraDAO, tieneDAO);
+        
+        // Inizializza Controller
+        ChefController chefController = new ChefController(gestioneChef);
+        
+        // Passa il controller alla GUI tramite setter statico
+        LoginChefGUI.setController(chefController);
+        
+        // Avvia JavaFX
+        Application.launch(LoginChefGUI.class, args);
+    }
 }
