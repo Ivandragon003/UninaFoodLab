c36fff270fa776967dbb62540fbc7ca621b0680a
Ivandragon003 <ivandefelice05@gmail.com>
2025-09-23 09:30:20 +0200

    db
    probema connesione db e connessione multiple

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,40 @@
+
+import Gui.LoginChefGUI;
+import controller.ChefController;
+import dao.*;
+import service.GestioneChef;
+import service.GestioneCorsiCucina;
+import javafx.application.Application;
 import util.DBConnection;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 public class Main {
-    public static void main(String[] args) {
-        try (Connection conn = DBConnection.getConnection()) {
-            PreparedStatement ps = conn.prepareStatement("SELECT NOW()");
-            ResultSet rs = ps.executeQuery();
-            if(rs.next()) {
-                System.out.println("Data/Ora DB: " + rs.getString(1));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            DBConnection.closeDataSource(); // chiude pool a fine programma
-        }
-    }
+	public static void main(String[] args) {
+		try {
+
+			ChefDAO chefDAO = new ChefDAO();
+			TieneDAO tieneDAO = new TieneDAO();
+			LavoraDAO lavoraDAO = new LavoraDAO();
+			CorsoCucinaDAO corsoDAO = new CorsoCucinaDAO();
+			IscrizioneDAO iscrizioneDAO = new IscrizioneDAO();
+			OnlineDAO onlineDAO = new OnlineDAO();
+			InPresenzaDAO inPresenzaDAO = new InPresenzaDAO();
+
+			GestioneChef chefService = new GestioneChef(chefDAO, tieneDAO, lavoraDAO);
+			GestioneCorsiCucina corsiService = new GestioneCorsiCucina(corsoDAO, chefDAO, tieneDAO, iscrizioneDAO,
+					onlineDAO, inPresenzaDAO);
+
+			ChefController chefController = new ChefController(chefService);
+
+			LoginChefGUI.setController(chefController, corsiService);
+
+			Application.launch(LoginChefGUI.class, args);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		} finally {
+
+			DBConnection.closeDataSource();
+		}
+	}
 }
--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -1,53 +1,32 @@
-
 package util;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class DBConnection {
 
-    private static HikariDataSource dataSource;
+	private static HikariDataSource ds;
 
-    static {
-        try {
-            HikariConfig config = new HikariConfig();
+	static {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(
+				"jdbc:postgresql://aws-1-eu-central-1.pooler.supabase.com:6543/postgres?sslmode=require&pgbouncer=true");
+		config.setUsername("postgres.knvggfhdcilpqhozvmkp");
+		config.setPassword("#federicoII");
+		config.setMaximumPoolSize(25);
+		ds = new HikariDataSource(config);
+	}
 
-            // URL JDBC Supabase PostgreSQL
-            config.setJdbcUrl("jdbc:postgresql://aws-1-eu-central-1.pooler.supabase.com:6543/postgres?sslmode=require&pgbouncer=true");
-            config.setUsername("postgres.knvggfhdcilpqhozvmkp");
-            config.setPassword("#federicoII");
+	public static Connection getConnection() throws SQLException {
+		return ds.getConnection();
+	}
 
-            // Configurazioni HikariCP
-            config.setMaximumPoolSize(5);       // max connessioni
-            config.setMinimumIdle(2);            // min connessioni sempre pronte
-            config.setIdleTimeout(30000);        // 30s per connessioni inattive
-            config.setConnectionTimeout(30000);  // timeout per ottenere connessione
-            config.setLeakDetectionThreshold(60000); // 60s per individuare connessioni non chiuse
+	public static void closeDataSource() {
+		if (ds != null && !ds.isClosed()) {
+			ds.close();
+		}
+	}
 
-            dataSource = new HikariDataSource(config);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Errore durante la configurazione di HikariCP", e);
-        }
-    }
-
-    /**
-     * Ottiene una connessione dal pool HikariCP
-     */
-    public static Connection getConnection() throws SQLException {
-        return dataSource.getConnection();
-    }
-
-    /**
-     * Chiude il pool quando l'applicazione termina
-     */
-    public static void closeDataSource() {
-        if (dataSource != null && !dataSource.isClosed()) {
-            dataSource.close();
-        }
-    }
 }
