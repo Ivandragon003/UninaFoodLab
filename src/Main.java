42235776cf496134651a36931888871308868edc
Matrbo7 <mattiacerbone7@gmail.com>
2025-09-23 09:14:28 +0200

    HikaryCP

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,20 @@
-import Gui.LoginChefGUI;
-import controller.ChefController;
-import dao.*;
-import service.GestioneChef;
-import service.GestioneCorsiCucina;
-import javafx.application.Application;
-import util.DBConnection;     // la tua classe DBConnection
-import java.sql.Connection;    // interfaccia JDBC Connection
+import util.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            // --- DEBUG: apertura connessione di prova ---
-            Connection testConn = DBConnection.getConnection(); // logga chi la apre
-
-            // --- Creazione DAO ---
-            ChefDAO chefDAO = new ChefDAO();
-            TieneDAO tieneDAO = new TieneDAO();
-            LavoraDAO lavoraDAO = new LavoraDAO();
-            CorsoCucinaDAO corsoDAO = new CorsoCucinaDAO();
-            IscrizioneDAO iscrizioneDAO = new IscrizioneDAO();
-            OnlineDAO onlineDAO = new OnlineDAO();
-            InPresenzaDAO inPresenzaDAO = new InPresenzaDAO();
-
-            // --- Creazione service ---
-            GestioneChef chefService = new GestioneChef(chefDAO, tieneDAO, lavoraDAO);
-            GestioneCorsiCucina corsiService = new GestioneCorsiCucina(
-                    corsoDAO, chefDAO, tieneDAO, iscrizioneDAO, onlineDAO, inPresenzaDAO
-            );
-
-            // --- Creazione controller principale ---
-            ChefController chefController = new ChefController(chefService);
-
-            // --- Passaggio controller e service alla GUI ---
-            LoginChefGUI.setController(chefController, corsiService);
-
-            // --- Avvio applicazione JavaFX ---
-            Application.launch(LoginChefGUI.class, args);
-
-            // --- DEBUG: chiusura connessione di prova ---
-            // DBConnection.close(testConn); // commenta se vuoi lasciarla aperta per test
-
+        try (Connection conn = DBConnection.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement("SELECT NOW()");
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                System.out.println("Data/Ora DB: " + rs.getString(1));
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            System.exit(1);
+        } finally {
+            DBConnection.closeDataSource(); // chiude pool a fine programma
         }
     }
 }
-
--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -1,26 +1,53 @@
+
 package util;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
 
 public class DBConnection {
 
-	private static final String URL = "jdbc:postgresql://aws-1-eu-central-1.pooler.supabase.com:6543/postgres?sslmode=require&pgbouncer=true";
-	private static final String USER = "postgres.knvggfhdcilpqhozvmkp";
-	private static final String PASSWORD = "#federicoII";
+    private static HikariDataSource dataSource;
 
-	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(URL, USER, PASSWORD);
-	}
+    static {
+        try {
+            HikariConfig config = new HikariConfig();
 
-	public static void close(Connection conn) {
-		if (conn != null) {
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+            // URL JDBC Supabase PostgreSQL
+            config.setJdbcUrl("jdbc:postgresql://aws-1-eu-central-1.pooler.supabase.com:6543/postgres?sslmode=require&pgbouncer=true");
+            config.setUsername("postgres.knvggfhdcilpqhozvmkp");
+            config.setPassword("#federicoII");
+
+            // Configurazioni HikariCP
+            config.setMaximumPoolSize(5);       // max connessioni
+            config.setMinimumIdle(2);            // min connessioni sempre pronte
+            config.setIdleTimeout(30000);        // 30s per connessioni inattive
+            config.setConnectionTimeout(30000);  // timeout per ottenere connessione
+            config.setLeakDetectionThreshold(60000); // 60s per individuare connessioni non chiuse
+
+            dataSource = new HikariDataSource(config);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Errore durante la configurazione di HikariCP", e);
+        }
+    }
+
+    /**
+     * Ottiene una connessione dal pool HikariCP
+     */
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    /**
+     * Chiude il pool quando l'applicazione termina
+     */
+    public static void closeDataSource() {
+        if (dataSource != null && !dataSource.isClosed()) {
+            dataSource.close();
+        }
+    }
 }
