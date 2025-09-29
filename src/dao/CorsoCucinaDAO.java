890e911075d6bc01fb50dd414e3c5bc2984ee3c6
Ivandragon003 <ivandefelice05@gmail.com>
2025-09-29 15:17:36 +0200

    bug
    sistemato dao per corso cucina che mette in automtico numero

--- src/dao/CorsoCucinaDAO.java
+++ src/dao/CorsoCucinaDAO.java
@@ -7,162 +7,162 @@ import util.DBConnection;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
 import java.util.Optional;
 import java.math.BigDecimal;
 
 public class CorsoCucinaDAO {
 
-	public int save(CorsoCucina corso) throws SQLException {
-		String sql = "INSERT INTO corsocucina "
-				+ "(nomeCorso, argomento, frequenzaCorso, prezzo, numeroPosti, dataInizioCorso, dataFineCorso) "
-				+ "VALUES (?, ?, ?::frequenza, ?, ?, ?, ?) RETURNING idCorsoCucina";
+    public int save(CorsoCucina corso) throws SQLException {
+        String sql = "INSERT INTO corsocucina "
+                + "(nomeCorso, argomento, frequenzaCorso, prezzo, numeroPosti, dataInizioCorso, dataFineCorso) "
+                + "VALUES (?, ?, ?::frequenza, ?, ?, ?, ?) RETURNING idCorsoCucina";
 
-		try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
 
-			ps.setString(1, corso.getNomeCorso());
-			ps.setString(2, corso.getArgomento());
-			ps.setString(3, corso.getFrequenzaCorso() != null ? corso.getFrequenzaCorso().name() : null);
-			ps.setBigDecimal(4, BigDecimal.valueOf(corso.getPrezzo()));
-			ps.setInt(5, corso.getNumeroPosti());
+            ps.setString(1, corso.getNomeCorso());
+            ps.setString(2, corso.getArgomento());
+            ps.setString(3, corso.getFrequenzaCorso() != null ? corso.getFrequenzaCorso().name() : null);
+            ps.setBigDecimal(4, BigDecimal.valueOf(corso.getPrezzo()));
+            ps.setInt(5, corso.getNumeroPosti());
 
-			if (corso.getDataInizioCorso() != null)
-			    ps.setTimestamp(6, Timestamp.valueOf(corso.getDataInizioCorso()));
-			else
-			    ps.setNull(6, Types.TIMESTAMP);
+            if (corso.getDataInizioCorso() != null)
+                ps.setTimestamp(6, Timestamp.valueOf(corso.getDataInizioCorso()));
+            else
+                ps.setNull(6, Types.TIMESTAMP);
 
-			if (corso.getDataFineCorso() != null)
-			    ps.setTimestamp(7, Timestamp.valueOf(corso.getDataFineCorso()));
-			else
-			    ps.setNull(7, Types.TIMESTAMP);
+            if (corso.getDataFineCorso() != null)
+                ps.setTimestamp(7, Timestamp.valueOf(corso.getDataFineCorso()));
+            else
+                ps.setNull(7, Types.TIMESTAMP);
 
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    int id = rs.getInt(1);
+                    corso.setIdCorso(id);
+                    return id;
+                }
+            }
+        }
+        return -1;
+    }
 
-			try (ResultSet rs = ps.executeQuery()) {
-				if (rs.next()) {
-					int id = rs.getInt(1);
-					corso.setIdCorso(id);
-					return id;
-				}
-			}
-		}
-		return -1;
-	}
+    public void update(CorsoCucina corso) throws SQLException {
+        String sql = "UPDATE corsocucina SET nomeCorso = ?, argomento = ?, frequenzaCorso = ?::frequenza, "
+                + "prezzo = ?, numeroPosti = ?, dataInizioCorso = ?, dataFineCorso = ? "
+                + "WHERE idCorsoCucina = ?";
 
-	public void update(CorsoCucina corso) throws SQLException {
-		String sql = "UPDATE corsocucina SET nomeCorso = ?, argomento = ?, frequenzaCorso = ?::frequenza, "
-				+ "prezzo = ?, numeroPosti = ?, dataInizioCorso = ?, dataFineCorso = ? " + "WHERE idCorsoCucina = ?";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, corso.getNomeCorso());
+            ps.setString(2, corso.getArgomento());
+            ps.setString(3, corso.getFrequenzaCorso() != null ? corso.getFrequenzaCorso().name() : null);
+            ps.setBigDecimal(4, BigDecimal.valueOf(corso.getPrezzo()));
+            ps.setInt(5, corso.getNumeroPosti());
 
-		try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+            if (corso.getDataInizioCorso() != null)
+                ps.setTimestamp(6, Timestamp.valueOf(corso.getDataInizioCorso()));
+            else
+                ps.setNull(6, Types.TIMESTAMP);
 
-			ps.setString(1, corso.getNomeCorso());
-			ps.setString(2, corso.getArgomento());
-			ps.setString(3, corso.getFrequenzaCorso() != null ? corso.getFrequenzaCorso().name() : null);
-			ps.setBigDecimal(4, BigDecimal.valueOf(corso.getPrezzo()));
-			ps.setInt(5, corso.getNumeroPosti());
+            if (corso.getDataFineCorso() != null)
+                ps.setTimestamp(7, Timestamp.valueOf(corso.getDataFineCorso()));
+            else
+                ps.setNull(7, Types.TIMESTAMP);
 
-			if (corso.getDataInizioCorso() != null)
-				ps.setTimestamp(6, Timestamp.valueOf(corso.getDataInizioCorso()));
-			else
-				ps.setTimestamp(6, null);
+            ps.setInt(8, corso.getIdCorso());
+            ps.executeUpdate();
+        }
+    }
 
-			if (corso.getDataFineCorso() != null)
-				ps.setTimestamp(7, Timestamp.valueOf(corso.getDataFineCorso()));
-			else
-				ps.setTimestamp(7, null);
+    public Optional<CorsoCucina> findById(int id) throws SQLException {
+        String sql = "SELECT * FROM corsocucina WHERE idCorsoCucina = ?";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next())
+                    return Optional.of(mapResultSetToCorso(rs));
+            }
+        }
+        return Optional.empty();
+    }
 
-			ps.setInt(8, corso.getIdCorso());
+    public List<CorsoCucina> getAll() throws SQLException {
+        List<CorsoCucina> list = new ArrayList<>();
+        String sql = "SELECT * FROM corsocucina ORDER BY nomeCorso";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
 
-			ps.executeUpdate();
-		}
-	}
+            while (rs.next()) {
+                CorsoCucina corso = mapResultSetToCorso(rs);
+                // Aggiorna numeroSessioni
+                corso.setNumeroSessioni(getNumeroSessioniPerCorso(corso.getIdCorso()));
+                list.add(corso);
+            }
+        }
+        return list;
+    }
 
-	public Optional<CorsoCucina> findById(int id) throws SQLException {
-		String sql = "SELECT * FROM corsocucina WHERE idCorsoCucina = ?";
-		try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+    public void delete(int id) throws SQLException {
+        String sql = "DELETE FROM corsocucina WHERE idCorsoCucina = ?";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        }
+    }
 
-			ps.setInt(1, id);
-			try (ResultSet rs = ps.executeQuery()) {
-				if (rs.next())
-					return Optional.of(mapResultSetToCorso(rs));
-			}
-		}
-		return Optional.empty();
-	}
+    public List<CorsoCucina> findByNomeOrArgomento(String filtro) throws SQLException {
+        List<CorsoCucina> list = new ArrayList<>();
+        String sql = "SELECT * FROM corsocucina WHERE nomeCorso ILIKE ? OR argomento ILIKE ? ORDER BY dataInizioCorso";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
 
-	public List<CorsoCucina> getAll() throws SQLException {
-		List<CorsoCucina> list = new ArrayList<>();
-		String sql = "SELECT * FROM corsocucina ORDER BY nomeCorso";
-		try (Connection conn = DBConnection.getConnection();
-				PreparedStatement ps = conn.prepareStatement(sql);
-				ResultSet rs = ps.executeQuery()) {
+            ps.setString(1, "%" + filtro + "%");
+            ps.setString(2, "%" + filtro + "%");
 
-			while (rs.next())
-				list.add(mapResultSetToCorso(rs));
-		}
-		return list;
-	}
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    CorsoCucina corso = mapResultSetToCorso(rs);
+                    corso.setNumeroSessioni(getNumeroSessioniPerCorso(corso.getIdCorso()));
+                    list.add(corso);
+                }
+            }
+        }
+        return list;
+    }
 
-	public void delete(int id) throws SQLException {
-		String sql = "DELETE FROM corsocucina WHERE idCorsoCucina = ?";
-		try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
-			ps.setInt(1, id);
-			ps.executeUpdate();
-		}
-	}
+    private CorsoCucina mapResultSetToCorso(ResultSet rs) throws SQLException {
+        String nome = rs.getString("nomeCorso");
+        double prezzo = rs.getDouble("prezzo");
+        String argomento = rs.getString("argomento");
+        String freqStr = rs.getString("frequenzaCorso");
+        Frequenza freq = freqStr != null ? Frequenza.valueOf(freqStr) : null;
+        int numeroPosti = rs.getInt("numeroPosti");
 
-	public List<CorsoCucina> findByNomeOrArgomento(String filtro) throws SQLException {
-	    List<CorsoCucina> list = new ArrayList<>();
-	    String sql = "SELECT * FROM corsocucina WHERE nomeCorso ILIKE ? OR argomento ILIKE ? ORDER BY dataInizioCorso";
-	    try (Connection conn = DBConnection.getConnection();
-	         PreparedStatement ps = conn.prepareStatement(sql)) {
-	        ps.setString(1, "%" + filtro + "%");
-	        ps.setString(2, "%" + filtro + "%");
-	        try (ResultSet rs = ps.executeQuery()) {
-	            while (rs.next()) {
-	                list.add(mapResultSetToCorso(rs));
-	            }
-	        }
-	    }
-	    return list;
-	}
+        CorsoCucina corso = new CorsoCucina(nome, prezzo, argomento, freq, numeroPosti);
+        corso.setIdCorso(rs.getInt("idCorsoCucina"));
 
+        Timestamp tsInizio = rs.getTimestamp("dataInizioCorso");
+        Timestamp tsFine = rs.getTimestamp("dataFineCorso");
+        if (tsInizio != null)
+            corso.setDataInizioCorsoFromDB(tsInizio.toLocalDateTime());
+        if (tsFine != null)
+            corso.setDataFineCorsoFromDB(tsFine.toLocalDateTime());
 
-	private CorsoCucina mapResultSetToCorso(ResultSet rs) throws SQLException {
-		String nome = rs.getString("nomeCorso");
-		double prezzo = rs.getDouble("prezzo");
-		String argomento = rs.getString("argomento");
+        return corso;
+    }
 
-		String freqStr = rs.getString("frequenzaCorso");
-		Frequenza freq = freqStr != null ? Frequenza.valueOf(freqStr) : null;
-
-		int numeroPosti = rs.getInt("numeroPosti");
-
-		CorsoCucina corso = new CorsoCucina(nome, prezzo, argomento, freq, numeroPosti);
-		corso.setIdCorso(rs.getInt("idCorsoCucina"));
-
-		Timestamp tsInizio = rs.getTimestamp("dataInizioCorso");
-		Timestamp tsFine = rs.getTimestamp("dataFineCorso");
-		if (tsInizio != null)
-			corso.setDataInizioCorsoFromDB(tsInizio.toLocalDateTime());
-		if (tsFine != null)
-			corso.setDataFineCorsoFromDB(tsFine.toLocalDateTime());
-
-		return corso;
-	}
-	
-	public int getNumeroSessioniPerCorso(int idCorso) throws SQLException {
-	    String sql = "SELECT COUNT(*) AS num_sessioni FROM sessione WHERE idcorsocucina = ?";
-	    try (Connection conn = DBConnection.getConnection();
-	         PreparedStatement ps = conn.prepareStatement(sql)) {
-	        ps.setInt(1, idCorso);
-	        try (ResultSet rs = ps.executeQuery()) {
-	            if (rs.next()) {
-	                return rs.getInt("num_sessioni");
-	            }
-	        }
-	    }
-	    return 0; 
-	}
-
-
+    // Ottiene solo il numero di sessioni per un corso
+    public int getNumeroSessioniPerCorso(int idCorso) throws SQLException {
+        String sql = "SELECT COUNT(*) AS num_sessioni FROM sessione WHERE idCorsoCucina = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, idCorso);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt("num_sessioni");
+                }
+            }
+        }
+        return 0;
+    }
 }
